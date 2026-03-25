c635513a248e77a081664931755e24565250ba3a
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-03-25 19:08:57 +0300

    Заметил фатальные ошибки
    Не знаю, почему он ранее магически не падал, но сейчас когда его запустил, то он начал падать из-за ошибок Scanner. Решил переписыванием некоторых вещей:
    1) Класс newOracle теперь наконец NewOracle
    2) Теперь вызов идёт метода start(Scanner scanner), а не newOracle()\oracle()
    3) Благодаря тому, что теперь метод start() принимает сканнер, теперь он вызывается лишь один раз, и уже затем закрывается. И теперь проект вновь работает

--- src/main/java/Oracle2/NewOracle.java
+++ src/main/java/Oracle2/NewOracle.java
@@ -7,8 +7,8 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
-public class newOracle {
-    public static void newOracle() throws InterruptedException {
+public class NewOracle {
+    public void start(Scanner scanner) throws InterruptedException {
         Path path = Paths.get("src\\main\\java\\Oracle2\\TextPrediction", "TextOracle");
 
         try {
@@ -18,13 +18,12 @@ public class newOracle {
             System.out.println(text.get(1));
             Thread.sleep(1000);
             System.out.print(text.get(2) + " ");
-            Scanner scanner = new Scanner(System.in);
             String userAnswer = scanner.nextLine().toLowerCase();
             boolean isValid = false;
             while (!isValid) {
                 switch (userAnswer) {
                     case "major" -> {
-                        scanner.close();
+
                         System.out.println(text.get(3));
                         Thread.sleep(1000);
                         System.out.println(text.get(4));
@@ -32,12 +31,13 @@ public class newOracle {
                         System.out.println(text.get(5));
                         isValid = true;
                         Major major = new Major();
+                        scanner.close();
                         System.out.println(major.getMajorCard());
                         System.out.println(major.getAsciiArt());
                         System.out.println(major.getMajorPrediction());
                     }
                     case "all" -> {
-                        scanner.close();
+
                         System.out.println(text.get(3));
                         Thread.sleep(1000);
                         System.out.println(text.get(4));
@@ -45,6 +45,7 @@ public class newOracle {
                         System.out.println(text.get(5));
                         isValid = true;
                         All all = new All();
+                        scanner.close();
                         System.out.println(all.getAllCard());
                         System.out.println(all.getAsciiArt());
                         System.out.println(all.getAllPrediction());
@@ -59,7 +60,7 @@ public class newOracle {
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
